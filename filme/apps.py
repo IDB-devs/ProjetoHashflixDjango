7cb37c7c5f5ea999056e2e8afb511794d71e0cd9
Igor Bortoleto <idbortoleto.arq@gmail.com>
2024-11-27 17:05:18 -0300

    adding superuser

--- filme/apps.py
+++ filme/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
+    
+    def ready(self): #criando um superuser para o deploy
+        from .models import Usuario
+        import os
+        
+        email = os.getenv('EMAIL_ADMIN') #criar variavel no servidor
+        senha = os.getenv('SENHA_ADMIN') #criar variavel no servidor
+        
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios: #se a lista estiver vazia
+            Usuario.objects.create_superuser(username='admin', email=email, password=senha,
+                                             is_active=True, is_staff=True)
